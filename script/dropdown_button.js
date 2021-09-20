cde7c23605f47d6471e92ee62cb771928d86ee5c
basicula <maksim.basum@gmail.com>
2021-09-20 21:13:21 +0300

    Add simple dropdown button element

---
+++ script/dropdown_button.js
@@ -0,0 +1,65 @@
+class DropDownButton {
+    constructor(header_text, on_open_callback, on_hide_callback) {
+        this.header_text = header_text;
+        this.on_open_callback = on_open_callback;
+        this.on_hide_callback = on_hide_callback;
+        this._init_element();
+    }
+
+    _init_element() {
+        // main container
+        this.element = document.createElement("div");
+
+        // button itself
+        this.button = document.createElement("div");
+        this.button.textContent = this.header_text;
+        this.is_opened = false;
+        this.element.appendChild(this.button);
+
+        // inner div for content can be replaced or extended
+        this.inner = document.createElement("div");
+        this.inner.style.display = "none";
+        this.element.appendChild(this.inner);
+
+        // optional arrow
+        this.arrow = document.createElement("i");
+        this.arrow.classList.add("fa");
+        this.arrow.classList.add("fa-caret-down");
+        this.button.appendChild(this.arrow);
+
+        // set additional callbacks
+        var self = this;
+        this.button.onclick = function() {
+            if (!self.is_opened) {
+                self.is_opened = true;
+                [self.inner.style.display, self.inner.prev_display] = [self.inner.prev_display, self.inner.style.display];
+
+                self.arrow.classList.add("fa-caret-up");
+                self.arrow.classList.remove("fa-caret-down");
+
+                if (self.on_open_callback)
+                    self.on_open_callback();
+            }
+            else {
+                self.is_opened = false;[self.inner.style.display, self.inner.prev_display] = [self.inner.prev_display, self.inner.style.display];
+
+                self.arrow.classList.add("fa-caret-down");
+                self.arrow.classList.remove("fa-caret-up");
+
+                if (self.on_hide_callback)
+                    self.on_hide_callback();
+            }
+        };
+    }
+
+    set_inner_content(inner_element) {
+        this.element.replaceChild(inner_element, this.inner);
+        this.inner = inner_element;
+        if (this.is_opened)
+            this.inner.prev_display = "none";
+        else {
+            this.inner.prev_display = this.inner.style.display;
+            this.inner.style.display = "none";
+        }
+    }
+}
